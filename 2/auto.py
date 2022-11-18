96a7d4a4a226116312ac93abf682f144046788ce
David Bertoldi (openmind) <david.bertoldi@openmindonline.it>
2022-11-18 11:16:02 +0100

    wip

--- 2/auto.py
+++ 2/auto.py
@@ -31,6 +31,7 @@ from sklearn.metrics import confusion_matrix
 
 
 
+
 BASE_URL = 'https://github.com/DBertazioli/multi-mnist_custom/raw/master/final_dataset/'
 SEED = 42
 np.random.seed(SEED)
@@ -51,15 +52,15 @@ def load_data():
 def build_autoencoder(input_shape, encoding_dim, activation):
 	input = Input(shape=input_shape)
 
-	downsampling_hidden_1 = Dense(512, activation = activation, name = "downsampling_hidden_1")(input)
-	downsampling_hidden_2 = Dense(256, activation = activation, name = "downsampling_hidden_2")(downsampling_hidden_1)
+	#downsampling_hidden_1 = Dense(512, activation = activation, name = "downsampling_hidden_1")(input)
+	downsampling_hidden_2 = Dense(256, activation = activation, name = "downsampling_hidden_2")(input)
 	downsampling_hidden_3 = Dense(128, activation = activation, name = "downsampling_hidden_3")(downsampling_hidden_2)
 
 	encoded = Dense(encoding_dim, activation=activation, name = "latent")(downsampling_hidden_3)
 
 	upsampling_hidden_1 = Dense(128, activation = activation, name = "upsampling_hidden_1")(encoded)
 	upsampling_hidden_2 = Dense(256, activation = activation, name = "upsampling_hidden_2")(upsampling_hidden_1)
-	upsampling_hidden_3 = Dense(256, activation = activation, name = "upsampling_hidden_3")(upsampling_hidden_2)
+	upsampling_hidden_3 = Dense(512, activation = activation, name = "upsampling_hidden_3")(upsampling_hidden_2)
 	
 	decoded = Dense(28*39, activation='sigmoid', name = "decoder")(upsampling_hidden_3)
 
@@ -90,9 +91,9 @@ def start():
 	X_test = X_test.reshape((X_test.shape[0], np.prod(X_test.shape[1:])))
 
 	#one-hot encoding
-	Y_train = np_utils.to_categorical(y_train)
-	Y_val = np_utils.to_categorical(y_val)
-	Y_test = np_utils.to_categorical(y_test)
+	Y_train = np_utils.to_categorical(y_train-1)
+	Y_val = np_utils.to_categorical(y_val-1)
+	Y_test = np_utils.to_categorical(y_test-1)
 
 
 	input_dims = np.prod(X_test.shape[1:]) #784
@@ -109,51 +110,77 @@ def start():
 	loss = 'binary_crossentropy'
 	dropout = 0.1
 
-	latent_size = 64
+	
 
 
-	encoded, decoded, autoencoder = build_autoencoder((input_dims,), latent_size, activation)
-	autoencoder.compile(optimizer=optimizer, loss=loss, metrics=['mse'])
+	d = []
 
 
-	e = 50
-	bs = 512
+	for cp in range(20, 31):
+
+		latent_size = 28*39//cp
+
+		encoded, decoded, autoencoder = build_autoencoder((input_dims,), latent_size, activation)
+		autoencoder.compile(optimizer=optimizer, loss=loss, metrics=['mse'])
 
 
-	history = autoencoder.fit(X_train,X_train,
-						epochs=e, 
-						batch_size=bs, 
-						validation_data = (X_val, X_val), 
-						callbacks = [])
+		e = 20
+		bs = 512
 
 
-	fig, axs = plt.subplots(4, 4)
-	rand = X_test[np.random.randint(0, 8000, 16)].reshape((4, 4, 1, 28*39))
+		history = autoencoder.fit(X_train,X_train,
+							epochs=e, 
+							batch_size=bs, 
+							validation_data = (X_val, X_val), 
+							callbacks = [])
+
+
+		score, acc = autoencoder.evaluate(X_test, X_test,
+                            batch_size=bs)
+
+		print('MSE:', acc)
+
+		d.append(acc)
+
+	df = pd.DataFrame(d)
+
+	#for k,v in d.items():
+	plt.plot(d)
+	#plt.legend()
+	plt.title('MSE vs compresion factor')
+	plt.ylabel('MSE')
+	plt.xlabel('Compression factor')
+	plt.xticks(np.arange(11), np.arange(20, 31))
+	plt.show()
+
+
+	#fig, axs = plt.subplots(4, 4)
+	#rand = X_test[np.random.randint(0, 8000, 16)].reshape((4, 4, 1, 28*39))
 
 	#display.clear_output() # If you imported display from IPython
 
-	for i in range(4):
-	    for j in range(4):
-	        axs[i, j].imshow(autoencoder.predict(rand[i, j])[0].reshape(28, 39), cmap = "gray")
-	        axs[i, j].axis("off")
+	#for i in range(4):
+	#    for j in range(4):
+	#        axs[i, j].imshow(autoencoder.predict(rand[i, j])[0].reshape(28, 39), cmap = "gray")
+	#        axs[i, j].axis("off")
 
-	plt.subplots_adjust(wspace = 0, hspace = 0)
-	plt.show()
+	#plt.subplots_adjust(wspace = 0, hspace = 0)
+	#plt.show()
 
 
-	n = 20
-	random_encodings = np.random.rand(20, latent_size)
-	decoded_imgs = Model(encoded, decoded).predict(random_encodings)
+	#n = 20
+	#random_encodings = np.random.rand(20, latent_size)
+	#decoded_imgs = Model(encoded, decoded).predict(random_encodings)
 
-	plt.figure(figsize=(20, 4))
-	for i in range(n):
-		# generation
-		ax = plt.subplot(2, n, i + 1 + n)
-		plt.imshow(decoded_imgs[i].reshape(28, 39))
-		plt.gray()
-		ax.get_xaxis().set_visible(False)
-		ax.get_yaxis().set_visible(False)
-	plt.show()
+	#plt.figure(figsize=(20, 4))
+	#for i in range(n):
+	#	# generation
+	#	ax = plt.subplot(2, n, i + 1 + n)
+	#	plt.imshow(decoded_imgs[i].reshape(28, 39))
+	#	plt.gray()
+	#	ax.get_xaxis().set_visible(False)
+	#	ax.get_yaxis().set_visible(False)
+	#plt.show()
 
 
 
