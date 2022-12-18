58d05970f0a780cc5467af946656d411085c1b2b
David Bertoldi (openmind) <david.bertoldi@openmindonline.it>
2022-12-18 11:43:38 +0100

    finale

--- 4/data.py
+++ 4/data.py
@@ -32,19 +32,24 @@ from tensorflow.keras.preprocessing import image
 from scipy.interpolate import make_interp_spline
 
 
-multiclass = np.array([[1000, 0, 0, 0, 0, 0],
-                       [0, 1000, 0, 0, 0, 0],
-                       [0, 0, 1000, 0, 0, 0],
-                       [0, 0, 0, 1000, 0, 0],
-                       [0, 0, 0, 0, 1000, 0],
-                       [0, 0, 0, 0, 0, 1000]])
+m = ["fc1", "block4_pool", "block3_pool"]
+x = np.arange(len(m))
+
+df = {'train': [0.99, 0.99, 0.99], 'test': [0.88, 0.86, 0.77]}
+
+plt.bar(x - 0.2, df["train"], width = 0.35, label = 'Train')
+plt.bar(x + 0.2, df["test"], width = 0.35, label = 'Test')
+
+for i in range(3):
+    plt.text(x = x[i]-0.3 , y = df["train"][i]+0.04, s = "%.2f" % df["train"][i], size = 10)
+    plt.text(x = x[i]+0.1 , y = df["test"][i]+0.04, s = "%.2f" % df["test"][i], size = 10)
+  
+plt.xticks(x, m)
+plt.ylim(top = 1.1)
+plt.xlabel("Models")
+plt.ylabel("Accuracy")
+plt.legend(loc = 'lower left', facecolor='white', framealpha=1)
 
 
-
-class_names = ['mountain', 'street', 'glacier', 'buildings', 'sea', 'forest']
-
-fig, ax = plot_confusion_matrix(conf_mat=multiclass,
-                                colorbar=True,
-                                figsize=(20,20),
-                                class_names=class_names, cmap='Blues')
+plt.subplots_adjust(bottom= 0.2, top = 0.98)
 plt.show()
